854a8eba694e43ecbd177499d0aee759f678818f
Jan Machala <jan.machala@email.cz>
2021-11-19 17:12:45 +0100

    Remove nulls from type when withoutNulls is called
    This should help phpstan to detect, that when withoutNulls is called on
    ArrayList<int|null> then resulting type is ArrayList<int>

---
+++ src/Bonami/Collection/Phpstan/ArrayListWithoutNullsReturnTypeExtension.php
@@ -0,0 +1,54 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Bonami\Collection\Phpstan;
+
+use Bonami\Collection\ArrayList;
+use PhpParser\Node\Expr\MethodCall;
+use PHPStan\Analyser\Scope;
+use PHPStan\Reflection\MethodReflection;
+use PHPStan\Type\DynamicMethodReturnTypeExtension;
+use PHPStan\Type\Generic\GenericObjectType;
+use PHPStan\Type\Type;
+use PHPStan\Type\TypeCombinator;
+
+class ArrayListWithoutNullsReturnTypeExtension implements DynamicMethodReturnTypeExtension
+{
+
+    public function getClass(): string
+    {
+        return ArrayList::class;
+    }
+
+    public function isMethodSupported(MethodReflection $methodReflection): bool
+    {
+        return $methodReflection->getName() === 'withoutNulls';
+    }
+
+    public function getTypeFromMethodCall(
+        MethodReflection $methodReflection,
+        MethodCall $methodCall,
+        Scope $scope
+    ): Type {
+        $type = $scope->getType($methodCall->var);
+
+        $declaringClassReflection = $methodReflection->getDeclaringClass();
+        $referencedClasses = $type->getReferencedClasses();
+
+        if (in_array(ArrayList::class, $referencedClasses, true)) {
+            $types = $declaringClassReflection->typeMapToList(
+                $declaringClassReflection->getTemplateTypeMap()->resolveToBounds()
+            );
+
+            return new GenericObjectType(
+                $declaringClassReflection->getName(),
+                array_map(static function (Type $type) {
+                    return TypeCombinator::removeNull($type);
+                }, $types)
+            );
+        }
+
+        return $type;
+    }
+}
